81651bc09e72a4788da020168c125257fcafa575
Kamau-sam <skamau.6411@gmail.com>
2024-04-10 19:27:31 +0300

    Added images folder

--- index.js
+++ index.js
@@ -1,43 +1,46 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const searchButton = document.getElementById('search-button');
-  const searchInput = document.getElementById('search-input');
-  const resultsSection = document.getElementById('results-section');
+document.addEventListener("DOMContentLoaded", () => {
+  const searchButton = document.getElementById("search-button");
+  const searchInput = document.getElementById("search-input");
+  const resultsSection = document.getElementById("results-section");
 
-  searchButton.addEventListener('click', function() {
-      const pokemonName = searchInput.value.toLowerCase().trim();
-      if (pokemonName) {
-          fetchPokemon(pokemonName);
-      } else {
-          alert('Please enter a Pokemon name');
-      }
+  searchButton.addEventListener("click", function () {
+    const pokemonName = searchInput.value.toLowerCase().trim();
+    if (pokemonName) {
+      fetchPokemon(pokemonName);
+    } else {
+      alert("Please enter a Pokemon name");
+    }
   });
 
   async function fetchPokemon(pokemonName) {
     try {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
-        if (!response.ok) {
-            throw new Error('Pokemon not found');
-        }
-        const data = await response.json();
-        displayResults(data);
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+      );
+      if (!response.ok) {
+        throw new Error("Pokemon not found");
+      }
+      const data = await response.json();
+      displayResults(data);
     } catch (error) {
-        alert(error.message);
+      alert(error.message);
     }
-}
-function displayResults(pokemon) {
-  resultsSection.innerHTML = ''; /
-  const pokemonCard = document.createElement('div');
-  pokemonCard.innerHTML = `
+  }
+  function displayResults(pokemon) {
+    resultsSection.innerHTML = "";
+    const pokemonCard = document.createElement("div");
+    pokemonCard.innerHTML = `
       <h2>${pokemon.name}</h2>
       <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}" />
       <p>Height: ${pokemon.height}</p>
       <p>Weight: ${pokemon.weight}</p>
       <button class="like-button">Like</button>
   `;
-  resultsSection.appendChild(pokemonCard);
-  const likeButton = pokemonCard.querySelector('.like-button');
-        likeButton.addEventListener('click', () => {
-            alert(`You liked ${pokemon.name}!`);
-        });
-    }
+    resultsSection.appendChild(pokemonCard);
+    const likeButton = pokemonCard.querySelector(".like-button");
+    likeButton.addEventListener("click", () => {
+      alert(`You liked ${pokemon.name}!`);
+      likeButton.style.backgroundColor = "yellow";
+    });
+  }
 });
